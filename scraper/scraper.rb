5c255197027e94f09783cf840a1987e5090f55ea
Jag <jagdhillon077@gmail.com>
2024-02-22 12:07:47 -0800

    removed unecessary context comments from scraper.rb

--- scraper/scraper.rb
+++ scraper/scraper.rb
@@ -1,14 +1,27 @@
 require "nokogiri"
 require "watir"
+require 'pp'
 
 BRANDS = [
+    'rolex',
+    'audemarspiguet',
+    'breitling',
     'iwc',
-    'rolex'
+    'jaegerlecoultre',
+    'omega',
+    'panerai',
+    'patekphilippe',
+    'cartier',
+    'gucci',
+    'seiko',
+    'movado',
+    'zenith'
 ]
 
 browser = Watir::Browser.new(:chrome)
 
 BRANDS.each do |brand|
+
     urls = [
         "https://www.chrono24.ca/#{brand}/index.htm?goal_suggest=1",
         "https://www.chrono24.ca/#{brand}/index.htm?goal_suggest=2",
@@ -18,31 +31,33 @@ BRANDS.each do |brand|
         "https://www.chrono24.ca/#{brand}/index.htm?goal_suggest=6"
     ]
 
+
     urls.each do |url|
         browser.goto("#{url}")
         sleep 2
         15.times do |i|
-            browser.execute_script("window.scrollBy(0,500)")
+            browser.execute_script("window.scrollBy(0,750)")
             sleep 1
         end
         doc = Nokogiri::HTML.parse(browser.html)
-        article_divs = doc.css(".article-item-container")
-        article_divs.each do |article_div|
-            image_div = article_div.at_css("js-carousel-cell carousel-cell d-flex is-selected .content img")
-            price_text = doc.css(".m-b-1 div .text-bold")[0].text.strip 
-            next if !image_div || !price_text 
-    
-            image_url = image_div['src']
-            price_number = price_text.gsub!(/[^0-9,]/, "")
-    
-            next if image_url.empty? || price.empty?
-    
+
+        doc.css("#wt-watches .article-item-container").each do |element|
+            next if !element
+
+            name = element.css(".text-bold")[0]&.children&.text
+            next if !name
+            pp name
+            image_url = element.css(".content img")[0]['src']
+            next if !image_url
+            pp image_url
+            price = element.css(".text-bold")[1].children.text
+            next if !price
+            price = price.gsub(/[^0-9]/, "")
+            pp price
+
             File.open("data/#{brand}.txt", "a+") do |f|
                 f.puts("#{image_url},#{price}")
-    
             end
         end
     end
-
-
 end
